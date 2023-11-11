746c52f5f9903dc0eaa599a842543c3964b8f2fd
Cloud <112786274+Potatooff@users.noreply.github.com>
2023-11-10 19:07:42 -0500

    Update test.js
    Optimization for empty text

--- js/test.js
+++ js/test.js
@@ -1,12 +1,19 @@
+// Shared variables
+const bgtask = document.getElementById("backg1");
+
+function EmptytextBg() 
+{
+    document.getElementById('backg2').innerHTML = ""; // Set that to empty
+    document.getElementById('backg3').innerHTML = ""; // Set that to empty
+}
+
 // Check local storage if page is reloaded! 
 window.onload = function () {
     let savedBgTask = sessionStorage.getItem('SavedBgTask');
     if (savedBgTask) {
-      const bgtask = document.getElementById('backg1');
       bgtask.innerHTML = savedBgTask; // Change bg text with task on local storage
       bgtask.style.overflow = 'visible'; bgtask.style.overflowWrap = 'break-word'; // reformat text from session storage
-      document.getElementById('backg2').innerHTML = ""; // Set that to empty
-      document.getElementById('backg3').innerHTML = ""; // Set that to empty
+      EmptytextBg()
     }
 }
 
@@ -16,12 +23,10 @@ document.addEventListener('keydown', (event) => {
 
         let userBgTask = prompt("Change Background Task: ");
         if (userBgTask !== null) {
-            const bgtask = document.getElementById("backg1")
             bgtask.innerHTML = userBgTask; // Change text on background
             bgtask.style.overflow = 'visible'; bgtask.style.overflowWrap = 'break-word'; // reformat text for live page
             sessionStorage.setItem('SavedBgTask', userBgTask); // Add task to session storage
-            document.getElementById('backg2').innerHTML = ""; // Set that to empty
-            document.getElementById('backg3').innerHTML = ""; // Set that to empty
+            EmptytextBg()
         } 
         else {console.log("Prompt canceled!");}
     }
