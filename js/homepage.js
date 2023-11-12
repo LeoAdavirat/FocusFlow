13000ef9020cebb29fdb3ae094fef03c16c7d24a
Cloud <christianabissi50@gmail.com>
2023-11-11 21:33:41 -0500

    bug fixes
    data json bug fixes :)

--- js/homepage.js
+++ js/homepage.js
@@ -1,24 +1,24 @@
 function readHomepageJson() {
-  return fetch("/json/homepage_json.json")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      console.error("Error fetching homepage JSON:", error);
-    });
+  return fetch("../json/homepage_json.json")
+    .then(res => res.json())
+    .then(data => {return data;});
 }
+
+
+
 function guideBackgroundNote() {
-  // document.getElementById("background-text-homepage").innerHTML =
-  //   "In the todo App, everything is organized into notes.\n\nEven background theme is also a note,\nwhich means you can edit it by pressing 'Shift + E'\n\nClick on add a note icon on the side to begin\n(For keyboard user: <b>Ctrl + Shift + A</b>)";
+  document.getElementById("background-text-homepage").innerHTML =
+    "In the todo App, everything is organized into notes.\n\nEven background theme is also a note,\nwhich means you can edit it by pressing 'Shift + E'\n\nClick on add a note icon on the side to begin\n(For keyboard user: <b>Ctrl + Shift + A</b>)";
   document.getElementById("background-text-homepage").innerHTML =
     data["background-text-homepage"];
 }
+async function getData() {
+const data = await readHomepageJson();
+console.log(data['background-text-homepage']);
 
-const data = readHomepageJson();
-console.log(data);
+}
+
+getData();
 // console.log(data);
 // document.getElementById("background-text-homepage").innerHTML =
 //   data["background-text-homepage"];
