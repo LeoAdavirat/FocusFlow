671d8c1d10f9f7706d12f1de9ee1f9b09a4e2ed3
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-11-12 09:21:21 +0700

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,18 @@
 function readHomepageJson() {
-  fetch("../json/homepage_json.json").then((res) => {
-    return res.json();
-  });
-  // .then((data) => console.log(data));
+  return fetch("/json/homepage_json.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching homepage JSON:", error);
+    });
 }
 function guideBackgroundNote() {
-  document.getElementById("background-text-homepage").innerHTML =
-    "In the todo App, everything is organized into notes.\n\nEven background theme is also a note,\nwhich means you can edit it by pressing 'Shift + E'\n\nClick on add a note icon on the side to begin\n(For keyboard user: <b>Ctrl + Shift + A</b>)";
+  // document.getElementById("background-text-homepage").innerHTML =
+  //   "In the todo App, everything is organized into notes.\n\nEven background theme is also a note,\nwhich means you can edit it by pressing 'Shift + E'\n\nClick on add a note icon on the side to begin\n(For keyboard user: <b>Ctrl + Shift + A</b>)";
   document.getElementById("background-text-homepage").innerHTML =
     data["background-text-homepage"];
 }
