29d6d1138e897b16c0ffc35b63e65bc6bd6b0f96
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-11-12 07:41:11 +0700

    splitted homepage and user jsons

--- write_json.py
+++ write_json.py
@@ -4,13 +4,18 @@ Even background theme is also a note,
 which means you can edit it by pressing 'Shift + E'
 
 Click on add a note icon on the side to begin
-(For keyboard user: <b>Ctrl + Shift + A</b>)""",
-"users":{
-    "background-text-homepage":""
+(For keyboard user: <b>Ctrl + Shift + A</b>)"""
 }
+
+user_dict = {
+    "username":"",
+    "password":"",
+    "background-text-homepage": "",
 }
 
 import json
 # json.dumps(a)
-with open("json/homepage.json", "w") as f:
+with open("json/homepage_json.json", "w") as f:
     f.write(json.dumps(a_dict))
+with open("json/user_data.json", "w") as f:
+    f.write(json.dumps(user_dict))
