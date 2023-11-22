490adecb3786e7cb9e4555e3fbb776c68115f911
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-11-22 15:17:59 +0700

    Improved landingPage

---
+++ js/landingPage.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scrollpos = localStorage.getItem("scrollpos");
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+window.onbeforeunload = function (e) {
+  localStorage.setItem("scrollpos", window.scrollY);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
+  document.getElementById("first-image").width =
+    window.innerWidth -
+    document.querySelector("#master-your-notes div").offsetWidth;
+});
