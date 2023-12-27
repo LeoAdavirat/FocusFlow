7c5a1dbe0c4dbece27d76d205a58705889ff59b4
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-12-27 20:51:24 +0700

    Logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 </script>
 
-<header class="bg-theme1-dock">
+<header class="bg-theme1-dock flex">
 	<h1 class="text-4.5xl text-theme1-text font-body font-bold text-center py-[6px]">FocusFlow</h1>
+	<div>Sign Up</div>
+	<div>Sign In</div>
 </header>
 <p class="text-1.5xl text-theme1-text font-body font-normal text-left pl-[22%] pt-[2.5%]"></p>
 
